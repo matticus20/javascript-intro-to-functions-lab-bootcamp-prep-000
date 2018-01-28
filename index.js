1dd6643c64405e5087ea58b925c802dca36d0229
Matthew Lugauskas <matthewlugauskas94@gmail.com>
2018-01-28 04:25:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ function logWhisper(string) {
   console.log(`${string}`)
 }
 logWhisper('hello')
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()
+    return "I can't hear you!";
+  else if string === string.toUpperCase()
+    return "YES INDEED!"
+  else if string === "I love you, Grandma."
+    return "I Love you, too."
+}
