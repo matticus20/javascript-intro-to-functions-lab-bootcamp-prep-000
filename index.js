a0d2f83cebc09ae077aa3488d4d5708dfd06f3dc
Matthew Lugauskas <matthewlugauskas94@gmail.com>
2018-01-28 04:33:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(`${string}`)
+  console.log(`${string}`).toUpperCase()
 }
-logShout('hello').toUpperCase()
+logShout('hello')
 
 function logWhisper(string) {
   console.log(`${string}`)
